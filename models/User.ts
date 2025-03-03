3ab17847457dcf25b403e3248f34a39cf3efa96b
Parth Saini <parthsaini@Parths-MacBook-Air.local>
2025-03-04 01:55:50 +0530

    models final setup

--- models/User.ts
+++ models/User.ts
@@ -1,39 +1,30 @@
-import mongoose, {model, Schema, models} from "mongoose";
+import mongoose, { Schema, model, models } from "mongoose";
 import bcrypt from "bcryptjs";
 
 export interface IUser {
-    email: string;
-    password: string;
-    role: "user" | "admin";
-    _id?: mongoose.Types.ObjectId;
-    createdAt?: Date;
-    updatedAt?: Date;
-}   
+  email: string;
+  password: string;
+  role: "user" | "admin";
+  _id?: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
 
-const userSchema = new Schema<IUser>({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        enum: ["user", "admin"],
-        default: "user"
-    }
-}, {timestamps: true});
+const userSchema = new Schema<IUser>(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  { timestamps: true }
+);
 
-//Hashing the password before saving in the database;
-userSchema.pre("save", async function(next) {
-    if(this.isModified("password")) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
-})
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
 
 const User = models?.User || model<IUser>("User", userSchema);
 
